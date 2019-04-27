dfa7e951f1c696bb4ca2124b8dbd0e7cb078cc1f
Mauro Camara Escudero <maurocamaraescudero@gmail.com>
2019-04-27 12:34:29 +0100

    refined plotting functions

--- utility_functions.py
+++ utility_functions.py
@@ -1,6 +1,8 @@
 import numpy as np
 from numpy.random import binomial
+from scipy.stats import multivariate_normal
 import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
 
 
 def sigmoid(x):
@@ -12,7 +14,7 @@ def sigmoid(x):
     :param x: Argument for sigmoid function. Can be float, list or numpy array.
     :type x: float or iterable.
     :return: Evaluation of sigmoid at x.
-    :rtype: float
+    :rtype: np.array
     """
     return 1.0 / (1.0 + np.exp(-x))
 
@@ -37,12 +39,15 @@ def lambda_func(x):
     return (sigmoid(x) - 0.5) / (2.0*x)
 
 
-def log_normal_kernel(t, m, v):
+def log_normal_kernel(t, m, v, multivariate=False):
     """
     This is basically the argument in the exponential of a normal distribution
     with mean=m and variance=v.
     """
-    return -0.5*np.log(2.0 * np.pi*m) - (((t - m) ** 2.0) / (2.0 * v))
+    if multivariate:
+        return -0.5*np.dot(t - m, np.dot(np.linalg.inv(v), t - m))
+    else:
+        return - (0.5*(t - m) ** 2.0) / v
 
 
 def generate_bernoulli(size, params):
@@ -59,40 +64,83 @@ def generate_bernoulli(size, params):
     return design, bin_samples
 
 
-def log_posterior(beta):
-    """
-    Numerically stable version of the log-posterior. Normalization constants
-    have been removed prior to log() transformation, as they would cancel out
-    anyways in Metropolis-Hastings.
-
-    :param beta:
-    :return:
-    """
-    # Find the prior distribution. Mean 0, Variance-Covariance 4n(X^T X)^-1
-    xbeta = np.dot(X, beta)
-    prior = - (0.125 * np.dot(xbeta, xbeta)) / n
-    log = np.dot(beta, np.sum(y.reshape(-1, 1) * X, axis=0)) - np.sum(np.log(1 + np.exp(xbeta)))
-    return prior + log
-
-
-def surface_plot(func, xleft, xright, yleft, yright, num):
+def surface_plot(f, xmin, xmax, ymin, ymax, n):
     """
     Given a R2 -> R function "func" which takes an array of dimension 2 and returns a scalar,
     this function will plot a surface plot over the specified region.
     """
-    b1 = np.linspace(xleft, xright, num)
-    b2 = np.linspace(yleft, yright, num)
-    B1, B2 = np.meshgrid(b1, b2)
-    Zbeta = np.array([func(beta) for beta in zip(B1.ravel(), B2.ravel())])
-    Zbeta = Zbeta.reshape(B1.shape)
+    # Get grid data
+    x, y, z = prepare_surface_plot(f, xmin, xmax, ymin, ymax, n)
+    # Plot 3D surface
     fig = plt.figure()
     ax = fig.add_subplot(111, projection='3d')
-    ax.plot_surface(B1, B2, Zbeta)
-    ax.set_xlabel('x')
-    ax.set_ylabel('y')
-    ax.set_zlabel('z')
-    plt.show()
-    return B1, B2, Zbeta
+    ax.plot_surface(x, y, z)
+    ax.update({'xlabel': 'x', 'ylabel': 'y', 'zlabel': 'z'})
+    return fig, ax, x, y, z
+
+
+def prepare_surface_plot(f, xmin, xmax, ymin, ymax, n):
+    """This function creates the three X, Y and Z arrays that are needed to
+    plot surfaces, but it does not plot them. It can be used to put multiple
+    plots into one figure."""
+    x, y = np.meshgrid(np.linspace(xmin, xmax, n), np.linspace(ymin, ymax, n))
+    z = np.array([f(xy) for xy in zip(x.ravel(), y.ravel())]).reshape(x.shape)
+    return x, y, z
+
+
+def row_outer(A):
+    """
+    Computes the row-wise outer product a_i a_i^T where each row is a_i.
+    """
+    return np.einsum('ni,nj->nij', A, A)
+
+
+def metropolis_multivariate(p, z0, cov, n_samples=100, burn_in=0, thinning=1):
+    """
+    Random-Walk Metropolis algorithm for a multivariate probability density.
+
+    :param p: Probability distribution we want to sample from.
+    :type p: callable
+    :param z0: Initial guess for Metropolis algorithm.
+    :type z0: np.array
+    :param cov: Variance-Covariance matrix for normal proposal distribution.
+    :type cov: np.array
+    :param n_samples: Number of total samples we want to obtain. This is the
+                      number of samples created after applying burn-in and
+                      thinning. Basically `samples[burn_in::thinning]`.
+    :type n_samples: int
+    :param burn_in: Number of samples to burn initially.
+    :type: burn_in: int
+    :param thinning: If `thinning` > 1 then after applying burn_in we get
+                     every <<thinning>> samples.
+    :type thinning: int
+    :return: `n_samples` samples from `p`.
+    :rtype: np.array
+    """
+    # Initialize algorithm. Calculate num iterations. Acceptance counter.
+    z, n_params, pz = z0, len(z0), p(z0)
+    tot = burn_in + (n_samples - 1) * thinning + 1
+    accepted = 0
+    # Init list storing all samples. Generate random numbers.
+    sample_list = np.zeros((tot, n_params))
+    logu = np.log(np.random.uniform(size=tot))
+    normal_shift = multivariate_normal.rvs(mean=np.zeros(n_params),
+                                           cov=cov, size=tot)
+    for i in range(tot):
+        # Sample a candidate from Normal(mu, sigma)
+        cand = z + normal_shift[i]
+
+        try:
+            # Store values to save computations. logu[i] <= 0 for u \in (0, 1)
+            p_cand = p(cand)
+            if p_cand - pz > logu[i]:
+                z, pz, accepted = cand, p_cand, accepted + 1
+        except (OverflowError, ValueError, RuntimeWarning):
+            continue
+
+        sample_list[i, :] = z
+    return sample_list[burn_in::thinning], accepted / tot
+
 
 
 def metropolis(p, scale, z0=None, n_samples=100, burn_in=0, thinning=1, log=False):
